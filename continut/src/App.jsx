abfe44458dd604ceff9a4ebb46331e57ebc054fb
deliasoare <deliasoare006@yahoo.com>
2023-11-23 23:40:10 +0200

    Create template

---
+++ continut/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Header from './components/Header.jsx'
+
+import './styles/styles.scss';
+function App() {
+
+  return (
+    <div className='content'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ continut/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className="icon">
+            </div>
+            <span className='title'>Perspective</span>
+            <GiHamburgerMenu className="hamburger" size={30} />
+        </div>
+    )
+}
+
+export default Header;
