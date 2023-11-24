4ae004648063917d078c4c74113b0c7c196d7044
Radulescu Razvan <proxy.razvan@gmail.com>
2023-11-24 17:42:39 +0200

    Commit

--- continut/src/App.jsx
+++ continut/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import Header from './components/Header.jsx'
+import Intro from './components/Intro.jsx'
 
 import './styles/styles.scss';
+
 function App() {
 
   return (
     <div className='content'>
       <Header />
+      <Intro />
     </div>
   )
 }
---
+++ continut/src/components/Intro.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Intro() {
+    return (
+        <div className="intro">
+            <img className="intro-bgimg" src="./coperta1.png" />
+        </div>
+    )
+}
