d32d29fd2d7a5a87722744f9a0316486714ae834
Radulescu Razvan <proxy.razvan@gmail.com>
2023-11-24 18:33:42 +0200

    Commit

--- continut/src/components/Intro.jsx
+++ continut/src/components/Intro.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react"
 
+function IntroText() {
 
+    const [textWidth, setTextWidth] = useState(100)
+
+    return (
+        <div className="intro-text">
+            <h2>
+                Descoperă lumile noi prin
+            </h2>
+            <h1>PERSPECTIVE</h1>
+
+            <button onClick={() => {
+                setTextWidth(200)
+            }} style={{ width: textWidth }}>
+                Află cum poți contribui!
+            </button>
+
+        </div>
+    )
+}
 
 export default function Intro() {
     return (
         <div className="intro">
+            <IntroText className="intro-text" />
+            <div className="intro-color-mask"/>
             <img className="intro-bgimg" src="./coperta1.png" />
         </div>
     )
