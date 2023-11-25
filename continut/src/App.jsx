d35fed59ebab639c22f4bd8a3d21382ad450c664
Radulescu Razvan <proxy.razvan@gmail.com>
2023-11-25 16:23:58 +0200

    Added new info

--- continut/src/App.jsx
+++ continut/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx'
 import Intro from './components/Intro.jsx'
 import About from './components/About.jsx';
 
+
 import './styles/styles.scss';
 
 function App() {
