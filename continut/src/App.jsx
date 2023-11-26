4ad7241aa7d7f7ce29a1f88db6d9407bd59d1e6c
deliasoare <deliasoare006@yahoo.com>
2023-11-26 19:34:49 +0200

    Add footer

--- continut/src/App.jsx
+++ continut/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
 
 import './styles/styles.scss';
 
@@ -11,8 +12,9 @@ function App() {
     <div className='content'>
       <Header />
       <Main />
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ continut/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="info">
+                <b>PERSPECTIVE</b>
+                <br></br>
+                copyright 2024
+            </div>
+            <div className="echipa">
+                Ioana Ana
+                <br></br>
+                Nichifor Darius
+                <br></br>
+                Rădulescu Răzvan
+                <br></br>
+                Soare Delia
+            </div>
+            <div className="contact">
+                <b>Contact 
+                    <br></br>
+                    +40 726 099 792
+                </b>
+            </div>
+        </div>
+    );
+
+}
+
+export default Footer; 
