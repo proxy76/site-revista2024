4c60e9006d5c16750aa3c883c6d604b8d274d89f
Radulescu Razvan <proxy.razvan@gmail.com>
2023-11-26 18:29:59 +0200

    Changed animation property

--- continut/src/components/About.jsx
+++ continut/src/components/About.jsx
@@ -16,11 +16,11 @@ export default function About() {
     const fadeInAnimationVariants = {
         initial: {
             opacity: 0,
-            y: 100,
+            x: -100,
         },
         animate: {
             opacity: 1,
-            y: 0,
+            x: 0,
             transition: {
                 delay: 0.5,
             }
