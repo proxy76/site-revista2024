b4f5697977b70d4d9910997889b1b319fb2490d7
Radulescu Razvan <proxy.razvan@gmail.com>
2023-11-30 19:39:19 +0200

    Modified some design elements

--- continut/src/components/Footer.jsx
+++ continut/src/components/Footer.jsx
@@ -5,8 +5,17 @@ function Footer() {
                 <b>PERSPECTIVE</b>
                 <br></br>
                 copyright 2024
+                <br></br>
+                <br></br>
+                Prof. coordonatori:
+                <br></br>
+                Darie Simona
+                <br></br>
+                Miu Livia
             </div>
             <div className="echipa">
+                Tehnoredactori:
+                <br></br>
                 Ioana Ana
                 <br></br>
                 Nichifor Darius
@@ -16,7 +25,9 @@ function Footer() {
                 Soare Delia
             </div>
             <div className="contact">
-                <b>Contact 
+                <img src="./wapp.png" alt="" />
+                <b>
+                    Contact 
                     <br></br>
                     +40 726 099 792
                 </b>
