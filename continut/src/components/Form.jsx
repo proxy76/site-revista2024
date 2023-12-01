fa6a18afb8d6d663db589f1a7dfd6660193baea1
Radulescu Razvan <proxy.razvan@gmail.com>
2023-12-01 12:19:00 +0200

    Changed form link

--- continut/src/components/Form.jsx
+++ continut/src/components/Form.jsx
@@ -6,7 +6,7 @@ const Form = forwardRef((props, contributeRef) => {
         <div ref={contributeRef} className="form">
             <h1>Contribuie și tu!</h1>
             <img src={Image} alt="" />
-            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdKuYfiiM-0GDaEYNJNAEWOED8YGIwiy4uEdk37xJyH6UsKwg/viewform">Mergi către form</a>
+            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfLUDItFhY5fyq2h_SMcCyNWoSYeD_e-F7plOTpWeF0d0f4Aw/viewform">Mergi către form</a>
         </div>
     );
 })
