4e2886dff3e2bf64f87cde1e723bdfa1fc9109f4
Radulescu Razvan <proxy.razvan@gmail.com>
2023-12-05 22:51:14 +0200

    .

--- continut/src/components/Form.jsx
+++ continut/src/components/Form.jsx
@@ -6,7 +6,7 @@ const Form = forwardRef((props, contributeRef) => {
         <div ref={contributeRef} className="form">
             <h1>Contribuie și tu!</h1>
             <img src={Image} alt="" />
-            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfLUDItFhY5fyq2h_SMcCyNWoSYeD_e-F7plOTpWeF0d0f4Aw/viewform">Mergi către form</a>
+            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfLUDItFhY5fyq2h_SMcCyNWoSYeD_e-F7plOTpWeF0d0f4Aw/viewform">Mergi către formular</a>
         </div>
     );
 })
