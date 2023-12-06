93c978e6616b108f0d0ac0c305244a159efb124f
Radulescu Razvan <proxy.razvan@gmail.com>
2023-12-06 21:24:48 +0200

    edit: styles and text overall

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ import { forwardRef } from 'react';
 const Form = forwardRef((props, contributeRef) => {
     return (
         <div ref={contributeRef} className="form">
-            {/* <p className='above-h1'>Anul 2001 - apariția primului număr <br></br> Anul 2024 - va apărea numărul 27 </p> */}
+            <p className='above-h1'>În decembrie 2001,  câțiva elevi din CNNG au redactat primul număr al revistei <b>Perspective</b>. În 2024 va apărea numărul 27. </p>
             <h1>Contribuie și tu!</h1>
             <img className='reviste' src="./reviste2.png" alt="" />
             <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfLUDItFhY5fyq2h_SMcCyNWoSYeD_e-F7plOTpWeF0d0f4Aw/viewform">Mergi către formular</a>
